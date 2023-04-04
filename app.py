2c07ec975d6a350317db618d8c9f9d90418c8d5f
Nikeesha <nikeeshaa@yahoo.com>
2023-04-04 10:50:33 +0530

    Flask application template fix

--- app.py
+++ app.py
@@ -2,17 +2,21 @@ from flask import Flask, render_template, send_file
 
 app = Flask(__name__)
 
-@app.route('/index')
+
+@app.route('/index.html')
 def home():
     return render_template('index.html')
 
-@app.route('/register')
+
+@app.route('/register.html')
 def register():
     return render_template('register.html')
 
-@app.route('/login')
+
+@app.route('/login.html')
 def login():
     return render_template('login.html')
+
 
 @app.route('/rest-project')
 def rest_project():
@@ -21,6 +25,7 @@ def rest_project():
                      as_attachment=True,
                      attachment_filename='Rest Project.ipynb')
 
+
 @app.route('/video-framing')
 def video_framing():
     return send_file('notebooks/video_Framing.ipynb',
@@ -28,12 +33,14 @@ def video_framing():
                      as_attachment=True,
                      attachment_filename='video_Framing.ipynb')
 
+
 @app.route('/model')
 def model():
     return send_file('notebooks/CNNModel.h5',
                      mimetype='application/json',
                      as_attachment=True,
                      attachment_filename='CNNModel.h5')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
